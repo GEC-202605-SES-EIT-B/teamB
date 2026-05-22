3f9f2c8d82d9a228a498a7866c14b69d22da1f62
厚汰 前澤 <kota.maezawa@ge-creative.co.jp>
2026-05-22 10:14:05 +0900

    テキスト文字変更

--- KakeiboApp/Form1.cs
+++ KakeiboApp/Form1.cs
@@ -88,7 +88,7 @@ namespace KakeiboApp
             dgvList.DataSource = null;
             dgvList.DataSource = AppData.Items;
 
-            MessageBox.Show("検索完了");
+            MessageBox.Show("集計完了");
         }
 
         private void label5_Click(object sender, EventArgs e)
@@ -120,7 +120,7 @@ namespace KakeiboApp
             dgvList.DataSource = null;
             dgvList.DataSource = data.ToList();
 
-            MessageBox.Show("集計完了");
+            MessageBox.Show("検索完了");
 
         }
     }
